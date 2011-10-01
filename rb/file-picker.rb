52555ea74597fe23e9b98ba1fe2a5ffc9daa10e8
spiralofhope <spiralofhope@lavabit.com>
2011-10-01 15:37:46 +0000

    Cleanup, restructure, more work.  It's shinier now.

--- rb/file-picker.rb
+++ rb/file-picker.rb
@@ -1,8 +1,5 @@
 #
 
-# TODO
-# In case I've run this back-to-back, don't move any new files if there are already some in there.
-
 
 # --
 # Problem
@@ -17,125 +14,138 @@
 # - Move the real files to that same specific directory.
 
 # For 'x' directories
-seek_directories =  [
-                      '/mnt/data/live/media/music/unsorted-tested',
-                      '/mnt/data/live/media/music/unsorted-untested',
+array_of_directories_to_search_through =  [
+                      '/mnt/data/live/media/music/unsorted-tested/',
+                      '/mnt/data/live/media/music/unsorted-untested/',
                     ]
-#seek_directories = [ '/mnt/data/live/media/music/playlist-testing' ]
-destination = '/mnt/data/live/media/music/playlist'
+destination_directory = '/mnt/data/live/media/music/playlist/'
 # 'x' files
 number_of_files = 3
-player_launch = '/usr/bin/deadbeef'
-player_command = [ '/usr/bin/deadbeef', '--queue' ]
 
-=begin
-seek_directories =  [
-                      '/mnt/ssd/projects/ruby/git/ruby-random/rb/tests',
-                      '/mnt/ssd/projects/ruby/git/ruby-random/rb/1',
-                    ]
-destination = '/mnt/ssd/projects/ruby/git/ruby-random/rb/d'
-number_of_files = 3
-player_launch = '/usr/bin/deadbeef'
-player_command = [ '/usr/bin/deadbeef', '--queue' ]
+def player_command_before( destination_directory )
+  pwd=Dir.pwd
+  Dir.chdir( destination_directory )
+  system( "export LS_COLORS=\"no=00:fi=00:di=00;36:ln=00;35:pi=40;33:so=00;35:bd=40;33;01:cd=40;33;01:or=00;05;37;41:mi=00;05;37;41:ex=00;32:*.cmd=00;32:*.exe=00;32:*.com=00;32:*.btm=00;32:*.bat=00;32:*.sh=00;32:*.csh=00;32:*.tar=00;31:*.tgz=00;31:*.arj=00;31:*.taz=00;31:*.lzh=00;31:*.rar=00;31:*.zip=00;31:*.z=00;31:*.Z=00;31:*.gz=00;31:*.bz2=00;31:*.bz=00;31:*.tz=00;31:*.rpm=00;31:*.cpio=00;31:*.torrent=00;31:*.jpg=00;35:*~.jpg=00;33:*.gif=00;35:*.bmp=00;35:*.xbm=00;35:*.xpm=00;35:*.png=00;35:*.tif=00;35:*~.mp3=00;03:*~.ogg=00;03:*~.flv=00;03:*~.ape=00;03:*~.flv=00;03:*~.mpg=00;03:*~.wmv=00;03:*.part=00;03:\";ssfm &" )
+  Dir.chdir( pwd )
+end
 
-# messy setup
-system( 'rm', '-rf', destination )
-system( 'mkdir',     destination )
-system( 'mkdir', '-p', 'tests/a/b' )
-files = [
-  '1/file1',
-  '1/file2',
-  '1/file3',
-  '1/file4',
-  'tests/tests-1',
-  'tests/tests-2',
-  'tests/tests-3',
-  'tests/tests-4',
-  'tests/a/file-a',
-  'tests/a/b/file-b',
-]
-# TODO:  I could implement a 'touch -p' which makes any missing directories.
-files.each{ |f|
-  system( 'touch', f )
-}
-=end
+def player_command_after( destination_directory )
+  # TODO:  Ensure deadbeef actually launches.  It has a habit of just dying on startup.
+  system( "/usr/bin/deadbeef #{ destination_directory } &" )
+end
 
-
-files = Array.new
-# TODO:  Sanity-check if 'seek_directories' has valid directories in it.
-seek_directories.each{ |dir|
-  # '*/*' means all directories and subdirectories, ignoring dot files.
-  files << Dir.glob( dir + '*/*' )
-}
-
-# TODO:  Sanity-check 'number_of_files'
-files = files.flatten.shuffle[ 0..( number_of_files-1 ) ]
-
-# For every file, create a still-working symlink to where it used to be.  Name it after the file, with '-dir'
-#   I need to have a reference, just in case it's an unnamed file from a directory which has the artist/album names.
-# Make symlinks to each file's original location.
-files.each{ |file|
-  source = File.dirname(  file )
-  # TODO?:  Also plant the last directory within the filename?
-  dest   = File.basename( file ) + '-dir'
-  dest   = File.join( destination, dest )
-  puts source + "\n  => " + dest
-  File.symlink( source, dest )
-}
+# --
 
 # I could do this without fileutils, but .. meh.
 #   http://www.ruby-doc.org/core/classes/File.html
 require 'fileutils'
 
-# TODO:  Sanity-check 'destination'
-# Create symbolic links.
-# http://www.ruby-doc.org/stdlib/libdoc/fileutils/rdoc/classes/FileUtils.html#M000899
-FileUtils::ln_s(
-  files,
-  destination,
-  #options = { :force => true }
-)
+# --
 
-# Rename them all to filename-link
-Dir.chdir( destination )
-Dir.glob( '*' ).each{ |file|
-  # Just in case.
-  next if not File.symlink?( file )
-  next if file[ -5..-1 ] == '-link'
-  #http://www.ruby-doc.org/stdlib/libdoc/fileutils/rdoc/classes/FileUtils.html#M000908
-  FileUtils::mv(
-    file,
-    file + '-link',
+def sanity_check(
+                  array_of_directories_to_search_through,
+                  destination_directory,
+                  number_of_files
+                )
+  # TODO:  Flesh this functionality out.
+  #
+  # In case I've run this back-to-back, don't move any new files if there are already any in there.
+  def files_exist?( destination_directory )
+    # Returns true if the named file is a directory, or a symlink that points at a directory, and false otherwise.
+    if File.directory?( destination_directory ) == false then
+      puts "Aborting:  The destination directory does not exist:\n  #{ destination_directory }"
+      return false
+    end
+    Dir.glob( destination_directory + '*' ).each{ |file|
+      if not File.symlink?( file ) == true then
+        puts "Aborting:  A non-symlink file already exists in:\n  #{ destination_directory }"
+        return false
+      end
+    }
+    return true
+  end
+  #
+  return false if files_exist?( destination_directory ) == false
+  return true
+  #
+end
+
+def pick_files( array_of_directories, number_of_files )
+  files = Array.new
+  array_of_directories.each{ |dir|
+    # '*/*' means all directories and subdirectories, ignoring dot files.
+    files << Dir.glob( dir + '*/*' )
+  }
+  # TODO:  Sanity-check 'number_of_files'
+  return files.flatten.shuffle[ 0..( number_of_files-1 ) ]
+end
+
+def create_source_symlinks( array_of_files, destination_directory )
+  # For every file, create a still-working symlink to where it used to be.  Name it after the file, with '-dir'
+  #   I need to have a reference, just in case it's an unnamed file from a directory which has the artist/album names.
+  # Make symlinks to each file's original location.
+  array_of_files.each{ |file|
+    source = File.dirname(  file )
+    # TODO?:  Also plant the last directory within the filename?
+    dest   = File.basename( file ) + '-dir'
+    dest   = File.join( destination_directory, dest )
+    #puts source + "\n  => " + dest
+    File.symlink( source, dest )
+  }
+end
+
+def link_files( array_of_files, destination_directory )
+  # TODO:  Sanity-check 'destination_directory'
+  # Create symbolic links.
+  # http://www.ruby-doc.org/stdlib/libdoc/fileutils/rdoc/classes/FileUtils.html#M000899
+  FileUtils::ln_s(
+    array_of_files,
+    destination_directory,
     #options = { :force => true }
   )
-}
+  #
+  # Rename them all to filename-link
+  Dir.chdir( destination_directory )
+  Dir.glob( '*' ).each{ |file|
+    # Just in case.
+    next if not File.symlink?( file )
+    next if file[ -5..-1 ] == '-link'
+    #http://www.ruby-doc.org/stdlib/libdoc/fileutils/rdoc/classes/FileUtils.html#M000908
+    FileUtils::mv(
+      file,
+      file + '-link',
+      #options = { :force => true }
+    )
+  }
+end # link_files( array_of_files, destination_directory )
 
-# Move the real files to that same specific directory.
-FileUtils::mv(
-  files,
-  destination,
-  #options = { :force => true }
-)
+def move_files( array_of_files, destination_directory )
+  # Move the real files to that same specific directory.
+  FileUtils::mv(
+    array_of_files,
+    destination_directory,
+    #options = { :force => true }
+  )
+end
 
-#system( player_launch )
+# --
 
-# TODO:  Ensure the player actually launches.  deadbeef has a habit of just dying on startup.
+player_command_before( destination_directory )
 
-# This wasn't working.  I'd have to fart around a lot more to feed each item from files_string into system()
-=begin
-# Play them
-files = Array.new
-Dir.glob( destination + '/*' ).each{ |file|
-  next if File.symlink?( file )
-  files << file
-}
-files.shuffle!.flatten!
+Kernel::exit( status=false ) if sanity_check(
+                          array_of_directories_to_search_through,
+                          destination_directory,
+                          number_of_files
+                        ) == false
 
-files_string = String.new
-files.each{ |file|
-  files_string << " '#{ file }'"
-}
-system( player_command, files_string )
-=end
+array_of_files = pick_files( array_of_directories_to_search_through, number_of_files )
 
-system( player_command, destination )
+# For every file, create a still-working symlink to where it used to be.  Name it after the file, with '-dir'
+create_source_symlinks( array_of_files, destination_directory )
+
+# The files will be moved shortly, breaking these symlinks.  This is normal and expected.
+link_files( array_of_files, destination_directory )
+
+move_files( array_of_files, destination_directory )
+
+player_command_after( destination_directory )
